41f58d03becc8a8d6e9dd0533a3e0cc3c26729ba
박진오 <jinoh1030@naver.com>
2025-08-20 18:09:27 +0900

    feat:프로젝트 초기 생성

---
+++ rare/src/test/java/com/jinoh/rareitemServer/RareitemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinoh.rareitemServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RareitemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
